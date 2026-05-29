533f844e1c3411c3722de1795abfe42f735aa3fe
jmayta1984 <jmayta@pucp.edu.pe>
2026-05-29 12:37:37 -0500

    feat: initialize Flutter project with basic structure and dependencies - Created main application structure with HomePage and product listing. - Implemented Product data transfer object (DTO) and repository pattern for fetching products. - Integrated HTTP package for API calls to fetch product data.

---
+++ lib/features/home/presentation/product_item.dart
@@ -0,0 +1,25 @@
+import 'package:easy_vet/features/home/domain/product.dart';
+import 'package:flutter/material.dart';
+
+class ProductItem extends StatelessWidget {
+  const ProductItem({super.key, required this.product});
+  final Product product;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            Image.network(product.image, height: 240),
+            Text(
+              product.name,
+              style: const TextStyle(fontWeight: FontWeight.bold),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
