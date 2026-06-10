153afdf24c7d02458ba95c12f842ddc39229b5f5
jmayta1984 <jmayta@pucp.edu.pe>
2026-06-10 12:27:22 -0500

    feat: remove unused provider import from login_page.dart

--- lib/features/auth/presentation/login_page.dart
+++ lib/features/auth/presentation/login_page.dart
@@ -3,7 +3,6 @@ import 'package:easy_vet/features/auth/presentation/login_view_model.dart';
 import 'package:easy_vet/features/main/presentation/main_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:provider/provider.dart';
 
 class LoginPage extends StatelessWidget {
   LoginPage({super.key});
