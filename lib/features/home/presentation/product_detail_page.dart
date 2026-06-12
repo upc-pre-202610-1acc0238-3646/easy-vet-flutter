4047d3d7164af851f1b3eff887084a4ef70b1efd
jmayta1984 <jmayta@pucp.edu.pe>
2026-06-12 11:46:30 -0500

    feat (home): enhance ProductDetailPage with quantity selection and action buttons

--- lib/features/home/presentation/product_detail_page.dart
+++ lib/features/home/presentation/product_detail_page.dart
@@ -4,20 +4,29 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 class ProductDetailPage extends StatelessWidget {
-  const ProductDetailPage({super.key, required this.product});
+  ProductDetailPage({super.key, required this.product});
   final Product product;
+  final ValueNotifier<int> quantity = ValueNotifier<int>(1);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        actions: [
+          IconButton(icon: const Icon(Icons.favorite_border), onPressed: () {}),
+          IconButton(icon: const Icon(Icons.share), onPressed: () {}),
+        ],
+      ),
       floatingActionButton: Padding(
         padding: const EdgeInsets.only(left: 16.0, right: 16.0),
         child: SizedBox(
           width: double.infinity,
           child: FloatingActionButton(
             onPressed: () {
-              context.read<CartViewModel>().addToCart(product.id, 1);
+              context.read<CartViewModel>().addToCart(
+                product.id,
+                quantity.value,
+              );
             },
             child: const Text('Add to cart'),
           ),
@@ -46,8 +55,32 @@ class ProductDetailPage extends StatelessWidget {
               Text(product.description),
               const SizedBox(height: 8),
               Text(
-                'Price: \$${product.price.toStringAsFixed(2)}',
+                '\$${product.price.toStringAsFixed(2)}',
                 style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+
+              Row(
+                children: [
+                  IconButton(
+                    icon: const Icon(Icons.remove),
+                    onPressed: () {
+                      if (quantity.value > 1) quantity.value--;
+                    },
+                  ),
+                  ValueListenableBuilder<int>(
+                    valueListenable: quantity,
+                    builder: (context, value, child) => SizedBox(
+                      width: 16,
+                      child: Center(child: Text('$value')),
+                    ),
+                  ),
+                  IconButton(
+                    icon: const Icon(Icons.add),
+                    onPressed: () {
+                      quantity.value++;
+                    },
+                  ),
+                ],
               ),
             ],
           ),
